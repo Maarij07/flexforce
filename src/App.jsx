2ca0e0f0062a725bea6c99eb054aa227e4afcb51
Maarij07 <221803@students.au.edu.pk>
2025-03-09 12:25:24 +0500

    scroll navigate position adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
@@ -7,10 +7,23 @@ import ContactUs from './components/ContactUs';
 import Teams from './components/Teams';
 import Events from './components/Events';
 import Collaborations from './components/Collaborations';
+import { useEffect } from 'react';
+
+function ScrollToTop() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Scroll to the top of the page every time the route changes
+    window.scrollTo(0, 0);
+  }, [location]); // Dependency on location means it will run on route change
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
